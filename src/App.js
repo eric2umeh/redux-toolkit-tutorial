460c43789f6c606d634959e04c511da8e77700d3
Eric <eric2umeh@yahoo.com>
2023-02-27 12:42:19 +0100

    Update the calculate total in App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import Navbar from './components/Navbar';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { calculateTotals, getCartItems } from './features/cart/cartSlice';
 
 function App() {
+  const { cartItems, isLoading } = useSelector((store) => store.cart);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
   return (
     <main>
       <Navbar />
