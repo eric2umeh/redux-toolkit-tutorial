e3ce192700b24c8f5f119fd8a5018e1c5a64c703
Eric <eric2umeh@yahoo.com>
2023-02-27 12:59:31 +0100

    Add modal and modalSlice to the App

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import Navbar from './components/Navbar';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { calculateTotals, getCartItems } from './features/cart/cartSlice';
+import Modal from './components/Modal';
+import CartContainer from './components/CartContainer';
 
 function App() {
   const { cartItems, isLoading } = useSelector((store) => store.cart);
   const dispatch = useDispatch();
+  const { isOpen } = useSelector((store) => store.modal);
 
   useEffect(() => {
     dispatch(calculateTotals());
@@ -13,7 +16,9 @@ function App() {
 
   return (
     <main>
+      {isOpen && <Modal />}
       <Navbar />
+      <CartContainer />
     </main>
   );
 }
